e232ad577bd994cd1a899b9aeb45adf7c47fbc51
limitgent <gentaznt@gmail.com>
2019-10-17 08:57:52 +0700

    tugas Web

---
+++ E41181324_Pravaasta_Genta_Arjunanda_Kinantaka/belajar2/contact.php
@@ -0,0 +1,14 @@
+
+    <div class="content">
+        <div class="main">
+            <h1>Hello Guys</h1> <br>
+            <p>Bagi kamu yang ingin tahu lebih lanjut mengenai mekanisme pembelajaran Bahasa Pemrograman. Web ini anda
+                dapat menghubungi kami melalui kontak yang tersedia. Kami dengan senang hati akan melakukan respond
+                pertanyaan anda. Terima Kasih.</p>
+        </div>
+        <div class="sidebar">
+            <h1>Contact Us</h1> <br>
+            <img src="img/img/twitter.png" alt="" width="50px">
+            <img src="img/img/facebook.png" alt="" id="fb" width="50px">
+        </div>
+    </div>
---
+++ E41181324_Pravaasta_Genta_Arjunanda_Kinantaka/belajar2/index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PacVic WEB</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <!--header-->
+    <header>
+        <a href="?page" id="logo">
+            <h1>
+                Hello Pakvic
+            </h1>
+            <h2>
+                Lecturer
+            </h2>
+        </a>
+
+        <nav>
+            <ul>
+                <li><a href="?page">Home</a></li>
+                <li><a href="?page=about">About</a></li>
+                <li><a href="?page=contact">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <!-- bagian untama -->
+    <section>
+        
+        <?php
+	error_reporting(0);
+	switch ($_GET['page']) {
+        case 'contact':
+			include "contact.php";
+			break;
+        case 'about':
+			include "about.php";
+			break;
+		default:
+			include "home.php";
+			break;
+				}
+	?>
+    </section>
+
+    <!-- footer -->
+    <footer>
+        <div class="social">
+            <ul>
+                <li><img src="img/fb.png" alt="facebook"></li>
+                <li><img src="img/twitter.jpg" alt="twitter"></li>
+            </ul>
+        </div>
+        <p>&copy; Copyright 2019 Hello Pakvic</p>
+    </footer>
+
+</body>
+
+</html>
