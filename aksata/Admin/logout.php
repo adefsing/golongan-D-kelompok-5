2c7126034129d4e1b43f710f0371e14ec312a32b
unknown <adefsing@gmail.com>
2019-10-18 15:04:06 +0700

    login and logout

---
+++ aksata/Admin/logout.php
@@ -0,0 +1,12 @@
+<?php 
+session_start();
+session_destroy();
+
+echo "<script>alert('Anda berhasil Logout');</script>";
+echo "<meta http-equiv='refresh' content='0; url=login.php'>";
+
+
+
+
+
+?>
