493b4fa9fde75cca54cfb83c67023c161d9751f5
ARDIANSYAH <m.aardiansyah17@gmail.com>
2019-11-08 00:52:11 +0700

    blajar array associative

--- E41181335_MOHAMMAD_AINUN_ARDIANSYAH/Belajar/array.php
+++ E41181335_MOHAMMAD_AINUN_ARDIANSYAH/Belajar/array.php
@@ -40,20 +40,71 @@ echo $campuran[0];
             border-radius : 50%;
         }
 
+        .clear{
+            clear : both;
+        }
+
     </style>
 </head>
 <body>
     
     <?php 
-    $angka = [[1,2,3],[4,5,6,7],[8,9,0]];
-  
+    $angka = [
+        [1,2,3],
+        [4,5,6,7],
+        [8,9,0]
+];
+    ?>
+
+    <?php
+    echo $angka [0][2];
+    echo "<br>";
     ?>
 
     <?php foreach($angka as $a) :?>
         <?php foreach($a as $b) :?>
             <div class="kotak"> <?= $b;?> </div>
         <?php endforeach; ?>
+        <div class="clear"></div> 
     <?php endforeach; ?>
 
+
+
+    <?php
+    // $didin = [["mohammad ainun ardiansyah","jember, 17 april 1999","tif"],
+    //             ["didin","jember","polije"]
+    //             ];
+
+    // array associative
+        $didin = [
+            [
+            "nama" => "mohammad ainun ardiansyah",
+            "asal" => "jember", 
+            "blabla" => "17 april 1999",
+            "sekolah" => "tif",
+            "nilai" => [100, 100, 100]
+            ],
+            [
+            "nama" => "mohammad ainun ardiansyah",
+            "asal" => "jember", 
+            "blabla" => "17 april 1999",
+            "sekolah" => "tif",
+            "nilai" => [100, 90, 100]
+            ]
+        ];
+
+        echo $didin [1] ["nilai"] [1];
+    ?>
+
+    <h1>data diri</h1>
+    <?php
+        foreach ($didin as $ansyah) : ?>
+
+    <ul>
+        <li>nama : <?= $ansyah ["nama"];?></li>
+        <li>blabla : <?= $ansyah ["asal"];?></li>
+        <li>sekolah : <?= $ansyah ["sekolah"];?></li>
+    </ul>
+    <?php endforeach;?>
 </body>
 </html>
