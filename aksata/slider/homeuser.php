cc196a80b8cc314c74a8aa6af3995fe34d446086
litaafia <litaafia13@gmail.com>
2019-11-20 08:09:11 +0700

    slider coba

---
+++ aksata/slider/homeuser.php
@@ -0,0 +1,100 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Hello, world!</title>
+  </head>
+  <body id="page-top">
+  <nav class="navbar navbar-expand-lg navbar-light bg-transparent fixed-top" id="mainNav">
+    <div class="container">
+  <a class="navbar-brand font-weight-bold text-white" href="Ppage-top">AKSATA</a>
+  <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link js-scroll-trigger text-white" href="#home">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link js-scroll-trigger text-white" href="#about">About</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link js-scroll-trigger text-white" href="#product">Product</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link js-scroll-trigger text-white" href="#gallery">Gallery</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link js-scroll-trigger text-white" href="#contact">Contact</a>
+      </li>
+    </ul>
+  </div>
+  </div>
+</nav>
+<section>
+<!-- <div class="jumbotron">
+  <div class="container"><h1 class="display-4">EXPLORE INDONESIA  WITH<br> <span class="font-weight-bold"> AKSATA</span> </h1>
+  <hr class="my-4">
+  <p class="lead">Mau Liburan Kemana?</p>
+  <a class="btn btn-primary btn-lg" href="#" role="button"> <span class="font-weight-bold">Pesan Paket</span> </a></div>
+</div>
+</section> -->
+
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="img/two.jpg" class="d-block w-100" alt="bali">
+      <div class="carousel-caption d-none d-md-block">
+      <h1 class="display-4">EXPLORE INDONESIA  WITH<br> <span class="font-weight-bold"> AKSATA</span> </h1>
+  <hr class="my-4">
+  <p class="lead">Mau Liburan Kemana?</p>
+  <a class="btn btn-primary btn-lg" href="#" role="button"> <span class="font-weight-bold">Pesan Paket</span> </a></div>
+        </div>
+    
+    <div class="carousel-item">
+      <img src="img/one.jpg" class="d-block w-100" alt="bali">
+      <div class="carousel-caption d-none d-md-block">
+      <h1 class="display-4">EXPLORE INDONESIA  WITH<br> <span class="font-weight-bold"> AKSATA</span> </h1>
+  <hr class="my-4">
+  <p class="lead">Mau Liburan Kemana?</p>
+  <a class="btn btn-primary btn-lg" href="#" role="button"> <span class="font-weight-bold">Pesan Paket</span> </a></div>
+        </div>
+   
+    <div class="carousel-item">
+      <img src="img/three.jpg" class="d-block w-100" alt="bali">
+      <div class="carousel-caption d-none d-md-block">
+      <h1 class="display-4">EXPLORE INDONESIA  WITH<br> <span class="font-weight-bold"> AKSATA</span> </h1>
+  <hr class="my-4">
+  <p class="lead">Mau Liburan Kemana?</p>
+  <a class="btn btn-primary btn-lg" href="#" role="button"> <span class="font-weight-bold">Pesan Paket</span> </a></div>
+        </div>
+    
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
